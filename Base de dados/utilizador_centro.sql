7438f7144d63af74094cc36b68d49c26d5c6fb58
Fate78 <63413401+Fate78@users.noreply.github.com>
2022-04-09 00:12:56 +0100

    script insert

---
+++ Base de dados/utilizador_centro.sql
@@ -0,0 +1,5 @@
+insert into utilizador_centro (id_utilizador, id_centro) values (204, 1);
+insert into utilizador_centro (id_utilizador, id_centro) values (205, 2);
+insert into utilizador_centro (id_utilizador, id_centro) values (219, 3);
+insert into utilizador_centro (id_utilizador, id_centro) values (221, 4);
+insert into utilizador_centro (id_utilizador, id_centro) values (227, 5);
