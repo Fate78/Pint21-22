e5d9c6a6edb2035b66867a5ccf418b7525d21cca
Fate78 <63413401+Fate78@users.noreply.github.com>
2022-04-08 23:18:19 +0100

    scripts create

---
+++ Base de dados/centro_geografico.sql
@@ -0,0 +1,5 @@
+insert into centro_geografico (nome_centro, ativo) values ('Tomar', 1);
+insert into centro_geografico (nome_centro, ativo) values ('Viseu', 1);
+insert into centro_geografico (nome_centro, ativo) values ('Fundão', 1);
+insert into centro_geografico (nome_centro, ativo) values ('Sines', 1);
+insert into centro_geografico (nome_centro, ativo) values ('Viana do Castelo', 1);
