88428b770b65355141dedf0f01a89e7539d8f2ab
Fate78 <63413401+Fate78@users.noreply.github.com>
2022-04-18 18:20:27 +0100

    Database updated
    Minor and major updates to database fields and data

--- Base de dados/utilizador_centro.sql
+++ Base de dados/utilizador_centro.sql
@@ -1,5 +1,5 @@
-insert into utilizador_centro (id_utilizador, id_centro) values (204, 1);
-insert into utilizador_centro (id_utilizador, id_centro) values (205, 2);
-insert into utilizador_centro (id_utilizador, id_centro) values (219, 3);
-insert into utilizador_centro (id_utilizador, id_centro) values (221, 4);
-insert into utilizador_centro (id_utilizador, id_centro) values (227, 5);
+insert into utilizador_centro (id_utilizador, id_centro) values (204, 1); #Gestor
+insert into utilizador_centro (id_utilizador, id_centro) values (205, 2); #Gestor
+insert into utilizador_centro (id_utilizador, id_centro) values (219, 3); #Gestor
+insert into utilizador_centro (id_utilizador, id_centro) values (221, 4); #Gestor
+insert into utilizador_centro (id_utilizador, id_centro) values (227, 5); #Gestor
