b291dd845d002ad1d6d7833a3775ef038adf0959
Fate78 <63413401+Fate78@users.noreply.github.com>
2022-04-26 21:55:02 +0100

    API update

--- RoomBookerAPI/RoomBookerAPI/Controllers/UtilizadoresController.cs
+++ RoomBookerAPI/RoomBookerAPI/Controllers/UtilizadoresController.cs
@@ -30,80 +30,34 @@ namespace RoomBookerAPI.Controllers
         }
 
         // GET: api/Utilizadores/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<Utilizador>> GetUtilizador(int id)
-        //{
-        //    var utilizador = await _context.Utilizadores.FindAsync(id);
-
-        //    if (utilizador == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return utilizador;
-        //}
+        [HttpGet("{id}")]
+        public ActionResult<Utilizador> GetUtilizador(int id)
+        {
+            return utilizadoresService.Get(id);
+        }
 
         // PUT: api/Utilizadores/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutUtilizador(int id, Utilizador utilizador)
-        //{
-        //    if (id != utilizador.IdUtilizador)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    _context.Entry(utilizador).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!UtilizadorExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return NoContent();
-        //}
+        [HttpPut("{id}")]
+        public Task<IActionResult> PutUtilizador(int id, Utilizador utilizador)
+        {
+            return utilizadoresService.Put(id, utilizador);
+        }
 
         //// POST: api/Utilizadores
         //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPost]
-        //public async Task<ActionResult<Utilizador>> PostUtilizador(Utilizador utilizador)
-        //{
-        //    _context.Utilizadores.Add(utilizador);
-        //    await _context.SaveChangesAsync();
-
-        //    return CreatedAtAction("GetUtilizador", new { id = utilizador.IdUtilizador }, utilizador);
-        //}
+        [HttpPost]
+        public Task<ActionResult<Utilizador>> PostUtilizador(Utilizador utilizador)
+        {
+            return utilizadoresService.Post(utilizador);
+        }
 
         //// DELETE: api/Utilizadores/5
         //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteUtilizador(int id)
+        //public Task<IActionResult> DeleteUtilizador(int id)
         //{
-        //    var utilizador = await _context.Utilizadores.FindAsync(id);
-        //    if (utilizador == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    _context.Utilizadores.Remove(utilizador);
-        //    await _context.SaveChangesAsync();
-
-        //    return NoContent();
+        //    return utilizadoresService.Delete(id);
         //}
 
-        //private bool UtilizadorExists(int id)
-        //{
-        //    return _context.Utilizadores.Any(e => e.IdUtilizador == id);
-        //}
     }
 }
--- RoomBookerAPI/RoomBookerAPI/Service/UtilizadoresService.cs
+++ RoomBookerAPI/RoomBookerAPI/Service/UtilizadoresService.cs
@@ -1,11 +1,17 @@
-﻿using RoomBookerAPI.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using RoomBookerAPI.Models;
+using RoomBookerAPI.Service;
+using RoomBookerAPI.Controllers;
+using System.Net;
+using System.Web.Http;
 
 namespace RoomBookerAPI.Service
 {
     public class UtilizadoresService : IUtilizadoresService
     {
         PintContext pintContext;
-
         public UtilizadoresService(PintContext context)
         {
             pintContext = context;
@@ -14,6 +20,70 @@ namespace RoomBookerAPI.Service
         public IEnumerable<Utilizador> GetAll()
         {
             return pintContext.Utilizadores;
+        }
+
+        public ActionResult<Utilizador> Get(int id)
+        {
+            var utilizador = pintContext.Utilizadores.Find(id);
+            //Check if null, return notfound
+            if(utilizador == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return utilizador;
+        }
+
+        public async Task<IActionResult> Put(int id, Utilizador utilizador)
+        {
+            if (id != utilizador.IdUtilizador)
+            {
+                return new BadRequestResult();
+            }
+
+            pintContext.Entry(utilizador).State = EntityState.Modified;
+
+            try
+            {
+                await pintContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UtilizadorExists(id))
+                {
+                    return new NotFoundResult();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new NoContentResult();
+        }
+        public async Task<ActionResult<Utilizador>> Post(Utilizador utilizador)
+        {
+            pintContext.Utilizadores.Add(utilizador);
+            await pintContext.SaveChangesAsync();
+
+            return new CreatedAtActionResult("GetUtilizador", "GetUtilizadores", new { id = utilizador.IdUtilizador }, utilizador);
+        }
+        //public async Task<IActionResult> Delete(int id)
+        //{
+        //    var utilizador = await pintContext.Utilizadores.FindAsync(id);
+        //    if (utilizador == null)
+        //    {
+        //        return new NotFoundResult();
+        //    }
+
+        //    pintContext.Utilizadores.Remove(utilizador);
+        //    await pintContext.SaveChangesAsync();
+
+        //    return new NoContentResult();
+        //}
+        private bool UtilizadorExists(int id)
+        {
+            return pintContext.Utilizadores.Any(e => e.IdUtilizador == id);
         }
     }
 }
