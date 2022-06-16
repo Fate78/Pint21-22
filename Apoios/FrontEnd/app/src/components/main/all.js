15a5ebe8db99f0771b0728f07c0de23f39637e60
Raster728 <rafael.figueiredo2001@gmail.com>
2022-06-16 15:52:43 +0100

    a

---
+++ Apoios/FrontEnd/app/src/components/main/all.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./header";
+import Menu from "./menu";
+
+
+
+
+const Panel = () => (
+    <div className="h-100 noScroll">
+        <Header />
+        <Menu />
+      
+    </div>
+)
+
+export default Panel
---
+++ Apoios/FrontEnd/app/src/components/main/menu.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Route} from "react-router-dom";
+import Inicio from './menu/Inicio';
+
+//Criar
+
+//Listar
+
+//Editar
+
+const Menu = () => (
+    
+
+    <div className="container-fluid h-100 noScroll" style={{padding: '0'}}>
+        
+    </div>
+);
+
+
+
+export default Menu;
