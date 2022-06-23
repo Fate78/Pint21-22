6a39819d7412708dcd3add96ec8ab5298e00bf2b
silveira99 <86411809+silveira99@users.noreply.github.com>
2022-06-23 16:18:16 +0100

    update app js

--- WebApp/PIIIIIINTFINAL/frontend/src/App.js
+++ WebApp/PIIIIIINTFINAL/frontend/src/App.js
@@ -2,12 +2,13 @@ import {BrowserRouter, BrowserRouter as Router, Route, Routes} from "react-route
 import './App.css';
 
 import Utilizadores from './menu/listar/utilizadores'
-
+import Notificacoes from './menu/listar/notificacoes'
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/utilizador' element={<Utilizadores />} />
+        <Route path='/tickets' element={< Notificacoes/>} />
       </Routes>
     </Router>
   );
