74e53d7fd77f6d7396041af12873f1be41736dd2
silveira99 <86411809+silveira99@users.noreply.github.com>
2022-08-28 15:18:42 +0100

    .

--- WebApp/PIIIIIINTFINAL/frontend/src/menu/login/login.js
+++ WebApp/PIIIIIINTFINAL/frontend/src/menu/login/login.js
@@ -1,75 +1,107 @@
-import React from 'react';
-import axios from 'axios';
-import { Link } from "react-router-dom"
-import '../../assets/bootstrap/bootstrap/css/bootstrap.css';
-import '../../assets/CSS/stylesdashboard1.css';
-import '../../assets/CSS/styleslogin.css';
+import React, { useEffect, useState, useRef } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import '../../CSS/stylesdashboard1.css';
+import '../../CSS/style.css'
+import axios from "axios"
+import useAuth from '../../hooks/useAuth';
+import { AuthProvider } from '../../context/AuthProvider';
 
-class Pagina extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
+const baseUrl = "https://roombookerapi.azurewebsites.net/api";
 
-  }
+export default function Pagina() {
+    const { setAuth } = useAuth();
 
-  componentDidMount() {
-    // get all entities - GET
-    fetch("https://roombookerapi.azurewebsites.net/api/utilizadores", {
-        "method": "GET",
-        "headers": {
-            "Accept": "application/json",
-            "Access-Control-Allow-Origin" : "true"
+    
+
+    const navigate = useNavigate();
+    const location = "/dashboard"
+
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, password])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post(baseUrl + "/utilizadores/authenticate",
+                {UtilizadorInput: user, PalavraPasse: password},
+                {
+                    Headers: { 'Content-Type': 'application/json' },
+                },
+            );
+            const { token } = response.data;
+            localStorage.setItem("token", token);
+            console.log(JSON.stringify(response?.data))
+            setUser('');
+            setPassword('');
+            setAuth({ user, password });
+            navigate(location, { replace: true });
+            
+            console.log(user, password)
+
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No server response');
+            } else if (err.response?.status === 400) {
+                setErrMsg('Missing username or password')
+            } else if (err.response?.status === 401) {
+                setErrMsg('Sem autorização')
+            } else {
+                setErrMsg('Login failed')
+            }
+            errRef.current.focus();
         }
-    })
-    .then(response => response.json())
-    .then(response => {
-        this.setState({
-        utilizadores: response
-        })
-    })
-    .catch(err => { console.log(err); 
-    });
+        
+    }
 
-}
-
-  render() {
     return (
-  
-            <div class="Section_top">
-             <div class=" center">
-                 <h1>Login</h1>
-                 <form method="post">
-                     <div class="txt_field">
-                         <input type="text" required />
-                         <label>Username</label> 
-                     </div>
-                     <div class="txt_field">
-                         <input type="password" required />
-                         <label>Password</label>
-                     </div>
-                     <div class= "pass">Esqueceu a Password? <a href="#">Recupere aqui</a></div>
-                      <a href="dashboard.html"><input type="submit" value="Login" /></a>
-                     <div class="signup_link">
-                         Faça Login ou registe-se <a href="registar.html">aqui</a>
-                     </div>
-                </form>
-             </div>
-            </div>
+        <div>
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive"> {errMsg} </p>
+            <h1>Entrar</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='password'> Nome de Utilizador:</label>
+                <input type='text'
+                    id='username'
+                    ref={userRef}
+                    autoComplete='off'
+                    onChange={(e) => setUser(e.target.value)}
+                    value={user}
+                    required
+                />
 
+                <label htmlFor='password'> Password:</label>
+                <input type='password'
+                    id='password'
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    required
+                />
+                <button>Entrar</button>
+            </form>
+            <p>
+                Queres criar uma conta?<br />
+                <span className='line'>
+                    {/*Router Link*/}
+                    <a href="#">Registar</a>
+                </span>
+            </p>
+        </div>
     );
-  }
+}
 
-  loadFillData() {
-    return this.state.utilizadores.map((data, index) =>{
-        return(
-            <tr key={index}>
-                
-                
-                    
-            </tr>
-        )
-    })
-}
-}
-export default Pagina;
+
+
+
